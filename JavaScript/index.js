4db50498e7b0ee370f56f23684440509c3c4fc44
Mochizue-RBT <raventejada1929@gmail.com>
2025-06-06 15:19:18 +0800

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,28 @@
+// // Practice javascript
+
+// // First set of variables
+
+// let a = prompt("What is your name?");
+// let b = "Raven";
+
+// alert("hello " + a + ", welcome to our store and the author of the website is " + b + " have fun shopping!");
+
+// // Second set of variables 
+
+// let c = prompt("What do you want for today?");
+
+// // Reverser variable
+
+// let d = a;
+//     a = c;
+//     c = d;
+
+// alert("Okay, " + a + " wants " + d + "!");
+// alert("Oh my bad, let me rephrase that real quick");
+// alert("Okay, " + d + " wants " + a + "!");
+
+// // Third set of variables
+
+// let e = "mochi";
+
+// alert("Please say hello to my dog " + e + ", " + d + "!");
